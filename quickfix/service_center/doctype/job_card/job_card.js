d2ee5dd4c367cfcfb900f9bfddb28f58a73d9335
Lokesh-287 <lokeshkrishna383@gmail.com>
2026-02-26 21:09:24 +0530

    feat: Complete Job Card Lifecycle

--- quickfix/service_center/doctype/job_card/job_card.js
+++ quickfix/service_center/doctype/job_card/job_card.js
@@ -1,8 +1,27 @@
 // Copyright (c) 2026, laptops, and tablets. Currently, everything runs on paper and WhatsApp. You will build their and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Job Card", {
-// 	refresh(frm) {
+frappe.ui.form.on("Job Card", {
+	refresh(frm) {
 
-// 	},
-// });
+	},
+    
+});
+
+frappe.ui.form.on("Part Usage Entry",{
+    quantity(frm,cdt,cdn){
+        calculate_total_price(frm,cdt,cdn)
+    },
+    unit_price(frm,cdt,cdn){
+        calculate_total_price(frm,cdt,cdn)
+    },
+    part_usage_entry_add(frm,cdt,cdn){
+        calculate_total_price(frm,cdt,cdn)  
+    }
+})
+
+function calculate_total_price(frm,cdt,cdn){
+    let row=locals[cdt][cdn]
+    let total_price=(row.unit_price||0)*(row.quantity|| 0 )
+    frappe.model.set_value(cdt,cdn,"total_price",total_price)
+}
