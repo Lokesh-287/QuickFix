1320407debef1ef9fc514baf58502c9e103e4370
Lokesh-287 <lokeshkrishna383@gmail.com>
2026-03-05 17:18:40 +0530

    feat: H3,H4

--- quickfix/service_center/doctype/job_card/job_card_list.js
+++ quickfix/service_center/doctype/job_card/job_card_list.js
@@ -1,6 +1,6 @@
 frappe.listview_settings['Job Card'] = {
 
-    add_fields: ["status"],
+    add_fields: ["status","final_amount", "priority"],
     has_indicator_for_draft:true,
 
     get_indicator(doc) {
@@ -29,6 +29,42 @@ frappe.listview_settings['Job Card'] = {
             return [("Cancelled"), "red", "status,=,Cancelled"];
         }
 
+    },
+     formatters: {
+        final_amount(value) {
+            if (!value) return value;
+            return `<b style="color:green">₹ ${value}</b>`;
+        }
+    },
+    button: {
+        show(doc) {
+            return doc.status === "In Repair";
+        },
+
+        get_label() {
+            return __("Complete");
+        },
+
+        get_description(doc) {
+            return __("Mark {0} as Ready for Delivery", [doc.name]);
+        },
+
+        action(doc) {
+            frappe.call({
+                method: "quickfix.api.mark_ready",
+                args: {
+                    job_card: doc.name
+                },
+                callback() {
+                    frappe.show_alert({
+                        message: "Job marked Ready for Delivery",
+                        indicator: "green"
+                    });
+                    frappe.listview.refresh();
+                }
+            });
+
+        }
     }
 
 };
